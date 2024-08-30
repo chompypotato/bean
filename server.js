9663ecb1455598538246407260e4d501e9ec36f6
chompypotato <fjhoch@students.wcpss.net>
2024-08-30 08:44:33 -0400

    Create server.js

---
+++ server.js
@@ -0,0 +1,28 @@
+const WebSocket = require('ws');
+const http = require('http');
+
+const server = http.createServer();
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', (ws) => {
+    console.log('Client connected');
+
+    ws.on('message', (message) => {
+        console.log('Received message:', message);
+
+        // Broadcast the message to all clients
+        wss.clients.forEach((client) => {
+            if (client !== ws && client.readyState === WebSocket.OPEN) {
+                client.send(message);
+            }
+        });
+    });
+
+    ws.on('close', () => {
+        console.log('Client disconnected');
+    });
+});
+
+server.listen(8080, () => {
+    console.log('WebSocket server is running on ws://localhost:8080');
+});
